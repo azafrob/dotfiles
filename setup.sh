84675fb5d7bce686c2be0c677f107dddd58e3b27
Adrian <adrian@gmail.com>
2025-06-26 10:23:25 +0200

    changes for arch

--- setup.sh
+++ setup.sh
@@ -6,20 +6,26 @@ sudo ./cachyos-repo.sh
 
 sudo pacman -Syu
 
-sudo pacman -S linux-cachyos linux-cachyos-headers cachyos-settings scx-scheds-git mesa-git mesa-utils yay power-profiles-daemon
+sudo pacman -S linux-cachyos linux-cachyos-headers cachyos-settings scx-scheds-git mesa-git mesa-utils yay power-profiles-daemon limine-entry-tool xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
 
-sudo pacman -S git zsh ghostty pulsemixer pamixer pavucontrol man-db tealdeer stow neovim downgrade bat fzf zoxide eza ripgrep fd fastfetch
+sudo pacman -S git fish ghostty man-db tealdeer stow neovim downgrade bat fzf zoxide eza ripgrep fd fastfetch atool flatpak timeshift yazi
 
-sudo pacman -S nwg-look kvantum waybar rofi-wayland hyprpaper libnotify qt5ct qt6ct qt5-wayland qt6-wayland
+sudo pacman -S nwg-look kvantum waybar hyprpaper dunst wofi libnotify qt6ct catppuccin-gtk-theme-mocha
 
-sudo pacman -S nerd-fonts noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-liberation ttf-dejavu ttf-roboto
-
-yay -S ttf-symbola
-
-sudo pacman -S snapper limine-snapper-sync limine-mkinitcpio-hook
+sudo pacman -S nerd-fonts noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
 
 sudo pacman -S firefox mangohud btop rocm-smi-lib
 
-sudo pacman -S cachyos-gaming-meta
+sudo pacman -S cachyos-gaming-meta cachyos-gaming-applications gamemode
+
+yay -S fan2go-git lact-git
 
 systemctl disable --now ananicy-cpp
+
+systemctl disable --now scx.service && systemctl enable --now scx_loader.service
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+tldr --update
+
+git clone https://github.com/catppuccin/Kvantum.git
