fe9c5d25397c57d88ecd75f61707e0b677756ce3
Adrian Zafra Robledo <adrian.zafrarobledo@gmail.com>
2025-12-11 15:32:59 +0100

    fix mesagit setup

--- scripts/setup_mesagit.sh
+++ scripts/setup_mesagit.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-cd && rm -rf mesa/ mesa-git/ && git clone https://gitlab.freedesktop.org/mesa/mesa
-cd mesa/
-meson setup build64 --libdir lib64 --prefix $HOME/mesa-git -Dgallium-drivers=radeonsi,zink,svga,softpipe,llvmpipe -Dvulkan-drivers=amd -Dbuildtype=release
+cd && rm -rf mesa-git-local/
+yay -G mesa-git
+cd mesa-git
+makepkg -so
+cd ./src/mesa
+meson setup build64 --libdir lib64 --prefix $HOME/mesa-git-local -Dgallium-drivers=radeonsi,zink,svga,softpipe,llvmpipe -Dvulkan-drivers=amd -Dbuildtype=release
 meson install -C build64
-cd && rm -rf mesa/
+cd && rm -rf mesa-git/
 sudo cp -rf ~/dotfiles/scripts/run-mesa-git /usr/bin/run-mesa-git
